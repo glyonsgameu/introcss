71790067e2569df5fe937b9f8ff06e855b6e8c90
gwl <gwlenglish@gmail.com>
2023-08-21 13:54:54 -0400

    Update progression.js

--- progression.js
+++ progression.js
@@ -3,6 +3,9 @@ function diminished_value(base, current, max){
     if (current >= max) {
         return 0;
     }
+    else if (current <=0) {
+        return base;
+    }
     
     var result = base - (base * (current/max));//linear
     return result;
